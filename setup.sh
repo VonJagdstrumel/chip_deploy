ecc90dea5eb78dbb905d0b4e29282399eec29437
Steve Forget <VonJagdstrumel@users.noreply.github.com>
2018-01-03 18:40:39 +0100

    Create an empty ssh_host_key if it doesn't exist in order to prevent host keys regeneration at each boot.
    OpenSSH seems to hijack rc.local at postinstall in order to check that host keys are generated. If they're not, it regenerates all of them at boot. ssh_host_key (rsa1) is still checked but can no longer be generated by ssh-keygen thus failing the check.

--- setup.sh
+++ setup.sh
@@ -156,6 +156,8 @@ setupFirewall() {
 setupSsh() {
     rm /etc/ssh/ssh_host_*
     ssh-keygen -A
+    touch /etc/ssh/ssh_host_key
+    chmod 600 /etc/ssh/ssh_host_key
 
     sed -ri 's/(PermitRootLogin) yes/\1 no/' /etc/ssh/sshd_config
     sed -ri 's/#(PasswordAuthentication) yes/\1 no/' /etc/ssh/sshd_config
